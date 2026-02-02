69f2b7bf65accd0f31cfdc85d5f315b9ac04c289
PalaiologosProgrammer <teutonic1121@gmail.com>
2026-02-02 21:00:31 +0400

    add admin panel for posts

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
 source "https://rubygems.org"
 
 gem 'devise'
-
 gem 'ransack'
+gem 'administrate'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 8.0.2"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :comments
+      resources :likes
+      resources :notifications
+      resources :posts
+      resources :users
+
+      root to: "comments#index"
+    end
   devise_for :users
   root "posts#index"
   resources :posts
